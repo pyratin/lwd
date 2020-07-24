86a053373dad40b5d759d92330ee15b364405b6b
pyratin <pyratin@hotmail.com>
2020-07-24 19:22:54 +0530

    base64TextComposited background black

--- js/server/schema/mutations/fns/gifGet.js
+++ js/server/schema/mutations/fns/gifGet.js
@@ -139,7 +139,7 @@ const gifGetFn = (
     ) => {
 
       const proc = exec(
-        'convert -loop 0 -delay 1000 miff:- -background black -alpha remove gif:-',
+        'convert -loop 0 -delay 1000 miff:- gif:-',
         {
           encoding: 'base64'
         },
@@ -186,7 +186,7 @@ const gifOptimizedGet = (
       );
 
       const proc = exec(
-        'convert gif:- -coalesce -fuzz 5% -layers OptimizeFrame +map gif:-',
+        'convert gif:- -coalesce gif:-',
         {
           encoding: 'base64'
         },
